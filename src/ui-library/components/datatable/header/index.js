ff0fcb27a42c6d053a15dca045609d4d46774828
= <=>
2021-08-16 13:35:54 +0530

    selectAll functionality added

--- src/ui-library/components/datatable/header/index.js
+++ src/ui-library/components/datatable/header/index.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React , {useState} from "react";
+import selectAllCheck from '../../../../../assets/images/select-all-check.svg';
+import selectAllUnCheck from '../../../../../assets/images/select-all-uncheck.svg';
 const Header = props => {
     const renderTableHeader = () => {
         if(props && props.columns && Array.isArray(props.columns) && props.columns) {
             return props.columns.map((key, index) => {
+                if(key.toLowerCase() === 'selectall') {
+                    if(props.selectAll) {
+                        return (
+                            <th key={index}>
+                                <img src={selectAllCheck} onClick={() => props.onSelectAll(false)} />
+                            </th>
+                        );
+                    } return (
+                        <th key={index}>
+                            <img src={selectAllUnCheck} onClick={() => props.onSelectAll(true)} />
+                        </th>
+                    )
+                }
                 return <th key={index}>{key.toUpperCase()}</th>
             })
         } return '';
--- src/ui-library/datatable/index.js
+++ src/ui-library/datatable/index.js
@@ -5,6 +5,7 @@ const DataTable = props => {
     const [data, setData] = useState(props.data);
     const [selectedKeys, setSelectedKeys] = useState([]);
     const [emptyMessage, setEmptyMessage] = useState('');
+    const [selectAll, setSelectAll] = useState(false);
     const getKeys = () => {
         const keys = Object.keys(props.data[0]);
         keys.unshift('selectall')
@@ -19,6 +20,16 @@ const DataTable = props => {
             setEmptyMessage(props.emptyMessage)
         }
     }
+    const onSelectAll = flag => {
+        if (flag === true) {
+            const ids = data.map(elem => elem.id);
+            setSelectedKeys([...ids]);
+            setSelectAll(flag);
+        } else {
+            setSelectedKeys([]);
+            setSelectAll(flag);
+        }
+    }
     const onSelect = id => {
         const updatedSelectedRows = selectedKeys;
         if (id && updatedSelectedRows && Array.isArray(updatedSelectedRows) &&
@@ -31,6 +42,7 @@ const DataTable = props => {
             setSelectedKeys([...updatedSelectedRows])
         }
     }
+    
     const renderTableData = () => {
         if(data && Array.isArray(data) && data.length) {
             const keys = getKeys();
@@ -54,7 +66,11 @@ const DataTable = props => {
         <>
             {!emptyMessage.trim().length ? 
                 <table id='datatable'>
-                    <TableHeader columns={getKeys()}/>
+                    <TableHeader
+                        columns={getKeys()}
+                        selectAll={selectAll}
+                        onSelectAll={onSelectAll}
+                    />
                     <tbody>
                         {renderTableData()}
                     </tbody>
